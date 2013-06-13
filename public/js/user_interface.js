fcab2b794a72ba710226d7637e8549a4e73a47d1
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 13:23:48 -0500

    created sliding function and saves state of movement in object

---
+++ public/js/user_interface.js
@@ -0,0 +1,31 @@
+var Track = function(name, length) {
+    this.name = name;
+    this.length = length;
+    this.start= 0;
+    this.stop= this.start + this.length;
+}
+
+Track.prototype = {
+  update: function(start) {
+    this.start = start;
+    this.stop = this.start + this.length;
+  }
+}
+
+$(document).ready(function() {
+
+  $('#add_track').click( function() {
+    var count = $('#track_list li').size();
+    var name = "track_" + count;
+    var track = new Track(name, 250);
+    $('#track_list').append('<li class="track_line"><div class="track"></div></li>');
+    $('.track').draggable( { containment: "parent" });
+   
+    $('li').mouseup( function() {
+      var start = parseInt($(this).find('.track').css("left"),10);
+      track.update(start);
+    });
+  });
+});
+
+
