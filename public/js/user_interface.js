a22f0b0399aa72efe255dafa25264b01dd36a28e
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 22:46:42 -0500

    sliding works with crop functionality and saves state

--- public/js/user_interface.js
+++ public/js/user_interface.js
@@ -2,14 +2,20 @@ $(document).ready(function() {
 
   playlist = new TrackList();
 
-
+  var CKEY = 67;
+  var SKEYPRESS = 115;
   var selectStart;
   var selectEnd;
-  var track;
   var selectedTrack;
 
   $('#add_track').click( addTrack );
-  $('#track_list').on("mousedown", ".track", select);
+
+  $(document).on("keypress", function(e) {
+      if ( e.which == SKEYPRESS) {
+        $('.track').draggable('disable');
+        $('#track_list').on("mousedown", ".track", select);
+      }
+    });
   $(document).on("keyup", cropSelection);
 
   function addTrack() {
@@ -17,31 +23,52 @@ $(document).ready(function() {
     var trackElement = $('<li class="track_line" id="track_'+track_index+'", data-track_index="'+track_index+'"><div class="track" style="visibility:hidden;"></div></li>');
     playlist.addTrack('tony_montana.wav', trackElement);
     $('#track_list').append(trackElement);
+    $('.track').draggable({ axis: "x" });
   }
 
   function select(e){
-    var parentOffset = $(this).parent().offset();
+    var parentOffset = $(this).offset();
     selectStart = e.pageX - parentOffset.left;
 
     selectedTrack = $(e.target.parentElement).data('track_index');
     console.log(selectedTrack);
 
     $(this).mouseup(function(e2){
-      var parentOffset = $(this).parent().offset();
+      var parentOffset = $(this).offset();
       selectEnd = e2.pageX - parentOffset.left;
+    });
+    enableDragging();
+  }
+
+  function enableDragging() {
+    $(document).on("keyup", function() {
+      $('.track').draggable('enable');
     });
   }
 
   function cropSelection(e) {
-    if ( event.which == 67 ) {
+    if ( event.which == CKEY ) {
+      
       track = playlist.tracks[selectedTrack];
       var selection = Math.abs(selectEnd - selectStart);
       track.offset = secondize(Math.min(selectStart, selectEnd));
+      console.log(track.offset + " offset");
+      console.log(track.startTime + " start")
+      track.startTime += secondize(Math.min(selectStart, selectEnd));
+      console.log(track.startTime + " afterstart")
       track.playTime = secondize(selection);
       track.render();
-      // debugger;
     }
   }
+
+  $('#track_list').on('mouseup', '.track', function(e) {
+    selectedTrack = $(e.target.parentElement).data('track_index');
+    var track = playlist.tracks[selectedTrack];
+    var left = parseInt($(this).css("left"), 10);
+    var startTime = secondize(left);
+    track.update(startTime);
+    track.render();
+  });
 });
 
 
