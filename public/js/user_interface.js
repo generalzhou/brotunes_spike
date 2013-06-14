f45a49ee83ff1fe724096a8c806b6e5177d344a2
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 17:23:03 -0500

    fixed merge conflict

--- public/js/user_interface.js
+++ public/js/user_interface.js
@@ -1,111 +1,120 @@
 $(document).ready(function() {
 
   playlist = new TrackList();
-  // Review why this function needs to exist. Out of place here
-  function playAll() { playlist.playAll(); }
 
+  function pixelize(seconds){
+    return seconds / playlist.longestDuration * 700;
+  }
+
+  function secondize(pixels){
+    return pixels / 700 * playlist.longestDuration;
+  }
 
   function UserInterface() {
-    function init() {
-      var context = new webkitAudioContext();
-
-    }
-
-  }
-  // Why not create a UserInterface object/class?
-  var CKEY = 67;
-  var SKEY = 83;
-  var selectStart;
-  var selectEnd;
+    var CKEY = 67;
+    var SKEY = 83;
+    var selectStart;
+    var selectEnd;
   // You're using this as the index at all times, but it's not named that way
   // Regardless, just save the track here, not the index.
-  var selectedTrack;
+    var selectedTrack;
 
-  // Two different approaches to the same callback...why?
-  $('#add_track').click( addTrack );
-  $('#play_all').on('click', playAll);
+    function init() {
+      var context = new webkitAudioContext();
+    }
 
-  function addTrack() {
-    // camelCase is standard in JS
-    var track_index = playlist.tracks.length;
-    // Pull these out into <script type="text/template"> elements on the HTML page, render using
-    // underscore.js' templating library
-    var trackElement = $('<li class="track_line" id="track_'+track_index+'", data-track_index="'+track_index+'"><div class="track"></div></li>');
-    trackElement.hide();
-    // Pull 'tony_montatna.wav' out to a variable as an attribute on the UserInterface object
-    playlist.addTrack('tony_montana.wav', trackElement);
+  // Review why this function needs to exist. Out of place here
+    function playAll() { playlist.playAll(); }
+    $('#add_track').click( addTrack );
+    $('#play_all').click( playAll);
 
-    $('#track_list').append(trackElement);
-    // Only call draggable on the new track elements
-    trackElement.find('.track').draggable({ axis: "x" });
-  }
+    function addTrack() {
+      var trackIndex = playlist.tracks.length;
+      // Pull these out into <script type="text/template"> elements on the HTML page, render using
+      // underscore.js' templating library
+      var trackElement = $('<li class="track_line" id="track_'+trackIndex+'", data-trackIndex="'+trackIndex+'"><div class="track"></div></li>');
+      // trackElement.hide();
+      // Pull 'tony_montatna.wav' out to a variable as an attribute on the UserInterface object
+      playlist.addTrack('tony_montana.wav', trackElement);
 
-  $('#track_list').on('mouseup', '.track', updateStartTime);
+      $('#track_list').append(trackElement);
+      // Only call draggable on the new track elements
+      trackElement.find('.track').draggable({ axis: "x" });
+    }
 
-  function updateStartTime(e) {
-    selectedTrack = $(e.target.parentElement).data('track_index');
-    var track = playlist.tracks[selectedTrack];
-    var left = parseInt($(this).css("left"), 10);
-    var startTime = secondize(left);
-    track.updateStartTime(startTime);
+    $('#track_list').on('mouseup', '.track', updateStartTime);
 
-    // Would be good have the track fire an 'changed' event and have the view pickup on the event
-    // which would then call track.render();
-    track.render();
-  };
+    function setSelectedTrack(e) {
+      var index = $(e.target.parentElement).data('trackIndex');
+      selectedTrack = playlist.tracks[index];
+    }
 
-  $(document).on("keydown", function(e) {
-    // write a descriptive method call which explains in plain english what's happening here
-    if ( e.which == SKEY) {
+    // make more descriptive
+    function updateStartTime(e) {
+      setSelectedTrack(e);
+      var left = parseInt($(this).css("left"), 10);
+      var startTime = secondize(left);
+      selectedTrack.updateStartTime(startTime);
+
+      // Would be good have the track fire an 'changed' event and have the view pickup on the event
+      // which would then call track.render();
+      selectedTrack.render();
+    }
+
+    $(document).on("keyup", keyUpEvent);
+    $(document).on("keydown", keyDownEvent);
+
+    function keyDownEvent(e) {
+      switch (e) {
+        case (e.which == SKEY):
+        createSoundSelection();
+        break;
+      }
+    }
+
+    function keyUpEvent(e) {
+      switch (e) {
+        case (e.which == CKEY):
+        cropSelection();
+        break;
+      }
+    }
+   
+    function createSoundSelection() {
       $('.track').draggable('disable');
-      $('#track_list').on("mousedown", ".track", select);
+      $('#track_list').on("mousedown", ".track", startSoundSelection);
       enableDragging();
     }
-  });
 
-  $(document).on("keyup", cropSelection);
-
-  $(document).on("keyup", function(e) {
-    if(e.code == "") {
-      cropSelection(e);
-
-    }
-  });
-
-  
-
-
-  // change this method's name
-  function select(e) {
-    var parentOffset = $(this).offset();
-    selectStart = e.pageX - parentOffset.left;
-    selectedTrack = $(e.target.parentElement).data('track_index');
-    $(this).mouseup(function(e2) {
+    // change this method's name
+    function startSoundSelection(e) {
+      setSelectedTrack(e);
       var parentOffset = $(this).offset();
-      selectEnd = e2.pageX - parentOffset.left;
-    });
-  }
-
-  function enableDragging() {
-    $(document).on("keyup", function() {
-      $('.track').draggable('enable');
-    });
-  }
-
-  function cropSelection(e) {
-    if ( event.which == CKEY ) {
-      // this will go away when selectedTrack is a track, not an index
-      track = playlist.tracks[selectedTrack];
-      var selection = Math.abs(selectEnd - selectStart);
-      var offset = secondize(Math.min(selectStart, selectEnd));
-      var startTime += secondize(Math.min(selectStart, selectEnd));
-      var playTime = secondize(selection);
-
-      track.cropPlayback( offset, startTime, playTime);
-      // this also can be removed after you start firing events for track changes
-      track.render();
+      selectStart = e.pageX - parentOffset.left;
+      $(this).mouseup( endSoundSelection );
     }
+
+    function endSoundSelection(e) {
+      var parentOffset = $(this).offset();
+      selectEnd = e.pageX - parentOffset.left;
+    }
+
+    function enableDragging() {
+      $(document).on("keyup", function() {
+        $('.track').draggable('enable');
+      });
+    }
+
+  //   function cropSelection() {
+  //     // this will go away when selectedTrack is a track, not an index
+  //     var selection = Math.abs(selectEnd - selectStart);
+  //     var offset = secondize(Math.min(selectStart, selectEnd));
+  //     var startTime += secondize(Math.min(selectStart, selectEnd));
+  //     var playTime = secondize(selection);
+
+  //     selectedTrack.cropPlayback( offset, startTime, playTime);
+  //     // this also can be removed after you start firing events for track changes
+  //     selectedTrack.render();
+  //   }
   }
 });
-
-
