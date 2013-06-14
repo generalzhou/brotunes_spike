ff36d09a5a3afcd9a7c4aa5e373100d59ce04acd
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 23:38:47 -0500

    debugged crop command

--- public/js/user_interface.js
+++ public/js/user_interface.js
@@ -3,20 +3,12 @@ $(document).ready(function() {
   playlist = new TrackList();
 
   var CKEY = 67;
-  var SKEYPRESS = 115;
+  var SKEY = 83;
   var selectStart;
   var selectEnd;
   var selectedTrack;
 
   $('#add_track').click( addTrack );
-
-  $(document).on("keypress", function(e) {
-      if ( e.which == SKEYPRESS) {
-        $('.track').draggable('disable');
-        $('#track_list').on("mousedown", ".track", select);
-      }
-    });
-  $(document).on("keyup", cropSelection);
 
   function addTrack() {
     var track_index = playlist.tracks.length;
@@ -24,41 +16,6 @@ $(document).ready(function() {
     playlist.addTrack('tony_montana.wav', trackElement);
     $('#track_list').append(trackElement);
     $('.track').draggable({ axis: "x" });
-  }
-
-  function select(e){
-    var parentOffset = $(this).offset();
-    selectStart = e.pageX - parentOffset.left;
-
-    selectedTrack = $(e.target.parentElement).data('track_index');
-    console.log(selectedTrack);
-
-    $(this).mouseup(function(e2){
-      var parentOffset = $(this).offset();
-      selectEnd = e2.pageX - parentOffset.left;
-    });
-    enableDragging();
-  }
-
-  function enableDragging() {
-    $(document).on("keyup", function() {
-      $('.track').draggable('enable');
-    });
-  }
-
-  function cropSelection(e) {
-    if ( event.which == CKEY ) {
-      
-      track = playlist.tracks[selectedTrack];
-      var selection = Math.abs(selectEnd - selectStart);
-      track.offset = secondize(Math.min(selectStart, selectEnd));
-      console.log(track.offset + " offset");
-      console.log(track.startTime + " start")
-      track.startTime += secondize(Math.min(selectStart, selectEnd));
-      console.log(track.startTime + " afterstart")
-      track.playTime = secondize(selection);
-      track.render();
-    }
   }
 
   $('#track_list').on('mouseup', '.track', function(e) {
@@ -69,6 +26,43 @@ $(document).ready(function() {
     track.update(startTime);
     track.render();
   });
+
+  $(document).on("keydown", function(e) {
+    if ( e.which == SKEY) {
+      $('.track').draggable('disable');
+      $('#track_list').on("mousedown", ".track", select);
+      enableDragging();
+    }
+  });
+
+  $(document).on("keyup", cropSelection);
+
+  function select(e) {
+    var parentOffset = $(this).offset();
+    selectStart = e.pageX - parentOffset.left;
+    selectedTrack = $(e.target.parentElement).data('track_index');
+    $(this).mouseup(function(e2) {
+      var parentOffset = $(this).offset();
+      selectEnd = e2.pageX - parentOffset.left;
+    });
+  }
+
+  function enableDragging() {
+    $(document).on("keyup", function() {
+      $('.track').draggable('enable');
+    });
+  }
+
+  function cropSelection(e) {
+    if ( event.which == CKEY ) {
+      track = playlist.tracks[selectedTrack];
+      var selection = Math.abs(selectEnd - selectStart);
+      track.offset = secondize(Math.min(selectStart, selectEnd));
+      track.startTime += secondize(Math.min(selectStart, selectEnd));
+      track.playTime = secondize(selection);
+      track.render();
+    }
+  }
 });
 
 
