fa0901d71becff4519280ae94a8c80bac40b4317
Nate Delage <nate@natedelage.com>
2013-06-14 15:15:23 -0500

    Review comments & refactorings

--- public/js/track.js
+++ public/js/track.js
@@ -1,3 +1,4 @@
+//Move these
 function pixelize(seconds){
   return seconds / playlist.longestDuration * 700;
 }
@@ -6,61 +7,85 @@ function secondize(pixels){
   return pixels / 700 * playlist.longestDuration;
 }
 
+// Move this to the UserInterface
 var context = new webkitAudioContext();
 
-function Track(url, elem, startTime, offset, playTime) {
-  this.buffer;
-  this.url = url;
-  this.startTime = typeof(startTime) !== 'undefined' ? startTime : 0;
-  this.offset = typeof(offset) !== 'undefined' ? offset : 0;
-  this.playTime;
-  this.duration;
-  this.elem = elem;
 
+function TrackView(el, track) {
+  sub("trackChanged", function(track) {
+    if( track == this ) {
+      this.render();
+    }
+  });
 
   this.render = function(){
     this.elem.children('.track').css("width", pixelize(this.playTime) + "px");
     this.elem.children('.track').css("left", pixelize(this.startTime) + "px");
     this.elem.children('.track').css("visibility",'');
   };
+}
+
+function Track(options) { //url, elem, startTime, offset, playTime) {
+  var defaults = {startTime: 0, offset: 0}
+  options = defaults.extend(options);
+
+  this.buffer;
+  // using the options hash instead of a long list of params
+  this.url = options.url;
+  this.startTime = typeof(startTime) !== 'undefined' ? startTime : 0;
+  this.offset = typeof(offset) !== 'undefined' ? offset : 0;
+  this.playTime;
+  this.duration;
+  this.elem = elem;
 
   this.setUpBuffer = function(){
-    source = context.createBufferSource();
+    var source = context.createBufferSource();
     source.buffer = this.buffer;
     return source;
   };
 
   this.connectNodes = function(source){
+    // Connect source to output (speakers)
     source.connect(context.destination);
   };
 
   this.play = function(){
     source = this.setUpBuffer();
     this.connectNodes(source);
-    source.noteGrainOn(this.startTime,this.offset,this.playTime);
+    source.start(this.delayUntilPlay,this.offset,this.playDuration);
   };
 
   this.loadSound = function() {
+    soundLoader.loadSound(url, this.soundLoaded)
     var thisTrack = this;
 
     var request = new XMLHttpRequest();
     request.open('GET', this.url, true);
     request.responseType = 'arraybuffer';
 
+    // What about errors? What if the URL doesn't exist or can't be parsed?
+    // Feature enhancment, spinner as you load URLs
     request.onload = function(){
       context.decodeAudioData(request.response, function(buffer){
         console.log("Back with the song");
-        thisTrack.buffer = buffer;
-        thisTrack.duration = buffer.duration;
-        thisTrack.playTime = buffer.duration;
-        thisTrack.render();
+        callback(buffer);
+
       });
     };
     request.send();
   };
 
-  this.update = function(startTime) {
-    this.startTime = startTime;
+
+  // This function is passed to the SoundLoader class, called when the sound has been loaded
+  this.soundLoaded(buffer) {
+    thisTrack.buffer = buffer;
+    thisTrack.duration = buffer.duration;
+    thisTrack.playTime = buffer.duration;
+    pub("trackLoaded", this);
+  }
+
+  this.updateDelayUntiPlay = function(startTime) {
+    this.delayUntilPlay = startTime;
   }
 
   this.loadSound();
--- public/js/track_list.js
+++ public/js/track_list.js
@@ -3,16 +3,29 @@ function TrackList(savedList){
   this.tracks = [];
   this.longestDuration = 10;
 
-  this.addTrack = function(url, elem, startTime, offset, playTime){
-    this.tracks.push(new Track(url,elem, startTime, offset, playTime));
-  };
+  // function save() {}
+  if (savedList !== undefined) {
+    this.load(savedList);
+  }
 
+
+  // Do this instead
+  this.addTrack = function(track) {
+    this.tracks.push(track);
+  }
+
+  //this.addTrack = function(url, elem, startTime, offset, playTime){
+  //  this.tracks.push(new Track(url,elem, startTime, offset, playTime));
+  //};
+
+  // Move this to UserInterface
   this.playAll = function(){
     for (i in this.tracks) {
       this.tracks[i].play();
     }
   };
 
+  // This is a LIE. It's not saving anything.
   this.save = function() {
     return JSON.stringify(this)
   };
@@ -20,7 +33,8 @@ function TrackList(savedList){
   this.load = function(json) {
     tracks = JSON.parse(json)['tracks'];
     for (i in tracks){
-      track = tracks[i]
+      var track = tracks[i]
+      // pass in options here, instead of the individual params
       this.tracks.push(new Track(track.url,
                                 track.startTime,
                                 track.trackOffset,
@@ -28,9 +42,5 @@ function TrackList(savedList){
                                 ));
     }
   };
-
-  if (savedList !== undefined) {
-    this.load(savedList);
-  }
 
 }
